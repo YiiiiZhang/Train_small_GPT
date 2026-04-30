7846b02f0e46e479c0cdf88868b0eefb92f8b437
HBen_zy <zylalala666@163.com>
2026-04-30 16:54:35 +0200

    trained bpe

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Train_small_GPT
+computation:
+- resource: A100 80G with 35H
+- A100: 312 TFLOPS, MFU:0.5 -> 140TFLOPS
+- total flops: 1.4*10^14 * 3600 * 35 = 1.76* 10^19 FLOPs
+
+- Chinchilla Scalling law: T ~ 20*N
+- flops need = 6*T*N -> N: 1B, T: 20B
+
+memory:
+- Model Weights(bf16 2 bytes): 2GB
+- Gradient: 2GB
+- AdamW(weight: fp32, 4bytes, Momentum: fp32, 4bytes, var: fp32, 4bytes - 12bytes):12GB 
--- src/model/transformer.py
+++ src/model/transformer.py
@@ -1,9 +1,8 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 from norm import RMSNorm
-from attention import precompute_freqs_cis,LLamaAttension
+from attention import LLamaAttension
 
 class Feedforward(nn.Module):
     def __init__(self, dim:int, hidden_dim:int):
@@ -30,37 +29,4 @@ class LlamaBlock(nn.Module):
         out = h+self.feed_forward(self.ffn_norm(h))
         return out
     
-class LlamaModel(nn.Module):
-    def __init__(self,vocab_size: int,dim:int,n_layers: int,n_heads:int, hidden_dim: int, max_seq_len: int = 512):
-        super().__init__()
-        self.vocab_size = vocab_size
-        self.max_seq_len = max_seq_len
-
-        #Token embeeding
-        self.tok_embeddings = nn.Embedding(vocab_size,dim)
-
-        #duplicate block
-        self.layers = [
-            LlamaBlock(dim,hidden_dim,hidden_dim) for _ in range(n_layers)
-        ]
-        self.norm = RMSNorm(dim)
-        self.output = nn.Linear(dim,vocab_size,bias=False)
-        freqs_cis = precompute_freqs_cis(dim // n_heads, max_seq_len)
-        self.register_buffer("freqs_cis", freqs_cis)
-    def forward(self,tokens: torch.Tensor, targets: torch.Tensor = None):
-        B,T = tokens.shape
-        #calculate embeedings
-        h = self.tok_embeddings(tokens)
-        #clip RoPE
-        freqs_cis = self.freqs_cis[:T]
-        
-        for layer in self.layers:
-            h = layer(h,freqs_cis)
-        h = self.norm(h)
-        logits = self.output(h)
-        loss = None
-        if targets is not None:
-            # 展平以便 CrossEntropyLoss 处理
-            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
-        return {"logits":logits,"loss":loss}
 
