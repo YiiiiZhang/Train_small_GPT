d39536358eb91ad08e51e0e7e882fae955ead25a
HBen_zy <zylalala666@163.com>
2026-05-05 21:39:49 +0200

    load datsets

--- src/DATA/load_datasets.py
+++ src/DATA/load_datasets.py
@@ -1,10 +1,10 @@
 from datasets import load_dataset
-
+import os
 # Set your local path here
 local_path = "/dss/dssmcmlfs01/pn25ju/pn25ju-dss-0000/Datasets" 
 datasets = ["roneneldan/TinyStories", "Skylion007/openwebtext"]
 # This will download and prepare the dataset in the specified folder
 for dataset_name in datasets:
     dataset = load_dataset(dataset_name)
-    dataset.save_to_disk(local_path)
+    dataset.save_to_disk(os.path.join(local_path, dataset_name.split("/")[1]))
     print("Dataset downloaded and stored at:", local_path)
