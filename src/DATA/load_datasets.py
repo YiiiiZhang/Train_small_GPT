586a9d7a42bc2ae9b25e91d7191356f18e0e0346
HBen_zy <zylalala666@163.com>
2026-05-05 21:48:40 +0200

    load datsets

--- src/DATA/load_datasets.py
+++ src/DATA/load_datasets.py
@@ -2,7 +2,7 @@ from datasets import load_dataset
 import os
 # Set your local path here
 local_path = "/dss/dssmcmlfs01/pn25ju/pn25ju-dss-0000/Datasets" 
-datasets = ["roneneldan/TinyStories", "Skylion007/openwebtext"]
+datasets = ["Skylion007/openwebtext"]
 # This will download and prepare the dataset in the specified folder
 for dataset_name in datasets:
     dataset = load_dataset(dataset_name)
